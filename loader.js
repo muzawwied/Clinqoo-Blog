84bfc72745fd862b39b7bdf8a0fcd7ae87958fe8
muzawwied <muzawwied@gmail.com>
2026-09-18 12:06:36 +0700

    blog: tambah 5 artikel coding 2026-09-18

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 // (sebelumnya sequential satu-satu — jauh lebih lambat karena menunggu tiap file selesai
 // sebelum mulai file berikutnya, padahal file-file data ini independen satu sama lain)
 (function() {
-  var files = ["data_panduan.js", "data_editor.js", "data_ai.js", "data_deploy.js", "data_tips.js", "data_coding.js", "data_coding_extra.js", "data_legal.js", "data_panduan_extra.js", "data_legal_extra.js", "data_editor_extra.js"];
+  var files = ["data_panduan.js", "data_editor.js", "data_ai.js", "data_deploy.js", "data_tips.js", "data_coding.js", "data_coding_extra.js", "data_legal.js", "data_panduan_extra.js", "data_legal_extra.js", "data_editor_extra.js", "data_coding_extra2.js"];
   var loaded = 0;
 
   function onFileDone() {
