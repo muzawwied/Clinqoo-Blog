20cb28ffc25cd153afffb26f8c5b57ea34d0cf7e
muzawwied <muzawwied@gmail.com>
2026-09-22 00:17:43 +0700

    blog: perbaiki merge data_analytics_extra2.js

--- data_analytics_extra2.js
+++ data_analytics_extra2.js
@@ -108,11 +108,11 @@
     }
   ];
   function merge(){
-    if (!window.countryDataFiles || !window.countryDataFiles[\"analytics\"]) {
+    if (!window.countryDataFiles || !window.countryDataFiles["analytics"]) {
       setTimeout(merge, 30);
       return;
     }
-    var arr = window.countryDataFiles[\"analytics\"].articles;
+    var arr = window.countryDataFiles["analytics"].articles;
     var have = {};
     for (var i = 0; i < arr.length; i++) have[arr[i].id] = true;
     for (var j = 0; j < extra.length; j++) {
